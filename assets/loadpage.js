b944596d0ca00b64cfb0c2d27f47bbee1731d74b
javonwashere <javonmcgilberry@gmail.com>
2021-08-30 00:16:27 -0400

    plus and minus signs added

--- assets/loadpage.js
+++ assets/loadpage.js
@@ -14,28 +14,38 @@ const accordions = document.querySelectorAll('.accordion-card');
 
 console.log('ACCORDIONS ', accordions);
 
-const openAccordion = (accordion) => {
+const openAccordion = (accordion, plus, minus) => {
   const content = accordion.querySelector('.accordion__content');
   accordion.classList.add('accordion__active');
   content.style.maxHeight = content.scrollHeight + 'px';
+  plus.classList.add('hide');
+  minus.classList.remove('hide');
+  console.log("PLUS SIGN", plus)
 };
 
-const closeAccordion = (accordion) => {
+const closeAccordion = (accordion, plus, minus) => {
   const content = accordion.querySelector('.accordion__content');
   accordion.classList.remove('accordion__active');
+  plus.classList.remove('hide');
+  minus.classList.add('hide');
   content.style.maxHeight = null;
 };
 
 accordions.forEach((accordion) => {
   const intro = accordion.querySelector('.accordion__intro');
   const content = accordion.querySelector('.accordion__content');
+  const accordionToggler = accordion.querySelector('.accordion__toggler');
+
+  let [plus, minus] = [accordionToggler.querySelector('.plus__sign'), accordionToggler.querySelector('.minus__sign')]
+
+
 
   intro.onclick = () => {
     if (content.style.maxHeight) {
-      closeAccordion(accordion);
+      closeAccordion(accordion, plus, minus);
     } else {
-      accordions.forEach((accordion) => closeAccordion(accordion));
-      openAccordion(accordion);
+      accordions.forEach((accordion) => closeAccordion(accordion, plus, minus));
+      openAccordion(accordion, plus, minus);
     }
   };
 });
